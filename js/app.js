c2213b7cabd75fdd8c7e4816f772a1642e22145f
swm92 <swm9220@gmail.com>
2019-10-10 12:17:13 -0700

    first draft

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function Players (name, age, gender, gold, placement) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.goldArray = [];
+    this.averagePlacement = placement; 
+}
+
+
